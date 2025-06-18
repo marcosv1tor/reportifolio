3005d55852ee4ef48476d22ba78e841996336ecb
Marcos Vítor <99407833+marcosv1tor@users.noreply.github.com>
2025-06-18 15:10:51 -0300

    Update ProjectsContainer.jsx

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 const ProjectsContainer = () => {
   return (
       <section className="projects-container">
-        <h2>Projetos</h2>
-          <p>
-            Todos os projetos que realizei e tive participação até o momento são para fins acadêmicos, de prática e aprendizado!
-          </p>
+        <h2>Projetos de fixação</h2>
           <a href="https://github.com/marcosv1tor?tab=repositories" target="_blank" className='btn'>
             Ver Projetos
           </a>
